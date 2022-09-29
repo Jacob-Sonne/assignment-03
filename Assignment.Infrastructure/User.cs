1370658b36692eece96c1129b42a21076a312c7a
Jacob Sonne <json@itu.dk>
2022-09-29 09:33:17 +0200

    IMPLEMENTED C# 1-3

--- Assignment.Infrastructure/User.cs
+++ Assignment.Infrastructure/User.cs
@@ -2,4 +2,15 @@ namespace Assignment.Infrastructure;
 
 public class User
 {
+    public int Id {get; set;}
+    
+    [Required]
+    [MaxLength(100)]
+    public string Name {get; set;}
+
+    [Required]
+    [MaxLength(100)]
+    public string Email {get; set;}
+
+    public IEnumerable<WorkItem> WorkItems {get; set;}
 }
--- Assignment.Infrastructure/WorkItem.cs
+++ Assignment.Infrastructure/WorkItem.cs
@@ -2,4 +2,18 @@ namespace Assignment.Infrastructure;
 
 public class WorkItem
 {
+    public int Id {get; set;}
+
+    [Required]
+    [MaxLength(100)]
+    public string Title {get; set;}
+
+    public User? AssignedTo {get; set;}
+
+    public string? Description {get; set;}
+
+    [Required]
+    public State State {get; set;}
+    
+    public IEnumerable<Tag> Tags {get; set;}
 }
