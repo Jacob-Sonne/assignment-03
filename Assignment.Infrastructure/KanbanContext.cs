5e0c1071e199f0588793bbdb6da9470b2bb9730b
Jacob Sonne <json@itu.dk>
2022-10-05 17:03:34 +0200

    save

--- Assignment.Infrastructure/KanbanContext.cs
+++ Assignment.Infrastructure/KanbanContext.cs
@@ -1,5 +1,32 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
 namespace Assignment.Infrastructure;
 
-public class KanbanContext
+public class KanbanContext : DbContext
 {
+    public KanbanContext(DbContextOptions<KanbanContext> options)
+            : base(options)
+        {
+        }
+    
+    public DbSet<User> Users {get; set;}
+    public DbSet<Tag> Tags {get; set;}
+    public DbSet<WorkItem> WorkItems {get; set;}
+    protected override void OnModelCreating(ModelBuilder modelBuilder){
+        modelBuilder.Entity<WorkItem>()
+            .HasOne(w => w.AssignedTo)
+            .WithMany(u => u.WorkItems);
+
+        modelBuilder.Entity<Tag>()
+            .HasIndex(t => t.Name)
+            .IsUnique();
+
+        modelBuilder.Entity<User>()
+            .HasIndex(u => u.Email)
+            .IsUnique();
+        
+    }
 }
--- Assignment.Infrastructure/Tag.cs
+++ Assignment.Infrastructure/Tag.cs
@@ -3,7 +3,9 @@ namespace Assignment.Infrastructure;
 public class Tag
 {
     public int Id {get; set;}
+    
     public string Name {get; set;}
+
     [Required]
     [MaxLength(50)]
     public IEnumerable<WorkItem> WorkItems {get; set;}
