d4ef87e1fdd0e6f1ce2cc41ded189a2536706976
Jacob Sonne <json@itu.dk>
2022-10-05 17:02:42 +0200

    save

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+var configuration = new ConfigurationBuilder()
+    .AddUserSecrets<Program>()
+    .Build();
+var connectionString = configuration.GetConnectionString("ConnectionString");
+
+var optionsBuilder = new DbContextOptionsBuilder<KanbanContext>();
+optionsBuilder.UseSqlServer(connectionString);
+
+var options = optionsBuilder.Options;
+
+Console.Write("Enter query to search for character: ");
+
+string? name;
+do
+{
+    Console.Write("Enter query to search for character: ");
+    name = Console.ReadLine();
+}
+while (string.IsNullOrWhiteSpace(name));
+
+using var context = new KanbanContext(options);
